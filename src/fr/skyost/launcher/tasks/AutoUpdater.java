ed2f1aaf1b9cf893758c272cb17396d20049a59b
Skyost <skyost@mail.com>
2014-06-26 16:20:33 +0200

    Updated AutoUpdater.

--- src/fr/skyost/launcher/tasks/AutoUpdater.java
+++ src/fr/skyost/launcher/tasks/AutoUpdater.java
@@ -1,7 +1,6 @@
 package fr.skyost.launcher.tasks;
 
 import java.io.File;
-import java.net.URLDecoder;
 import java.util.logging.Level;
 
 import fr.skyost.launcher.LauncherConstants;
@@ -20,7 +19,16 @@ public class AutoUpdater extends Thread {
 			if(Utils.compareVersions(remoteVersion, LauncherConstants.LAUNCHER_VERSION)) {
 				LogUtils.log(Level.INFO, LauncherConstants.AUTO_UPDATER_PREFIX + "An update was found : " + remoteVersion + ".");
 				LogUtils.log(Level.INFO, LauncherConstants.AUTO_UPDATER_PREFIX + "Downloading it...");
-				ConnectionUtils.download(LauncherConstants.LATEST_VERSION_JAR, new File(URLDecoder.decode(Skyolauncher.class.getProtectionDomain().getCodeSource().getLocation().getPath().replace(".jar", "-update.jar"), "UTF-8")));
+				String path = Skyolauncher.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+				path = path.substring(0, path.lastIndexOf(".jar")) + " v" + remoteVersion + ".jar";
+				final File destination = new File(path);
+				if(destination.exists()) {
+					final File bak = destination;
+					bak.renameTo(new File(destination.getAbsolutePath() + "-bak"));
+					destination.createNewFile();
+				}
+				ConnectionUtils.download(LauncherConstants.LATEST_VERSION_JAR, destination);
+				LogUtils.log(Level.INFO, LauncherConstants.AUTO_UPDATER_PREFIX + "Done. The updated launcher's path is : " + path + ".");
 			}
 			else {
 				LogUtils.log(Level.INFO, LauncherConstants.AUTO_UPDATER_PREFIX + "No update found.");
